4565e41d10caa1f24d14d334a35c07b553a9502e
drdr xp <drdr.xp@gmail.com>
2012-04-22 23:35:30 +0800

    add format_line() to format multi rows in single line

--- strutil/strutil.py
+++ strutil/strutil.py
@@ -18,3 +18,43 @@ def line_pad(linestr, padding=''):
     lines = "\n".join(lines)
 
     return lines
+
+
+def format_line(items, sep=' ', aligns=''):
+
+    listtype = (type([]), type(()))
+
+    aligns = [x for x in aligns] + [''] * len(items)
+    aligns = aligns[:len(items)]
+    aligns = ['r' if x == 'r' else x for x in aligns]
+
+    items = [(x if type(x) in listtype else [x])
+             for x in items]
+
+    items = [[str(y) for y in x]
+             for x in items]
+
+    maxHeight = max([len(x) for x in items])
+
+    max_width = lambda x: max([len(y) for y in x])
+    widths = [max_width(x) for x in items]
+
+    items = [(x + [''] * maxHeight)[:maxHeight]
+             for x in items]
+
+    lines = []
+    for i in range(maxHeight):
+        line = []
+        for j in range(len(items)):
+            width = widths[j]
+            elt = items[j][i]
+            if aligns[j] == 'l':
+                elt = elt.ljust(width)
+            else:
+                elt = elt.rjust(width)
+            line.append(elt)
+        line = sep.join(line)
+
+        lines.append(line)
+
+    return "\n".join(lines)
