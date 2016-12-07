82b3f162da286d68d815044efad2996cca1e4fd8
drdr xp <drdr.xp@gmail.com>
2016-12-07 19:27:43 +0800

    Squashed 'mysqlconnpool/' content from commit 163d883
    git-subtree-dir: mysqlconnpool
    git-subtree-split: 163d883e6fec5fc8fbd2c4a519ab6e4e5de2f6e5

---
+++ mysqlconnpool.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python2
+# coding: utf-8
+
+import copy
+import MySQLdb
+import Queue
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+class MysqlConnectionPool(object):
+
+    def __init__(self, conn_argkw, options=None):
+
+        options = options or {}
+        options = copy.deepcopy(options)
+
+        self.options = options
+        self.queue = Queue.Queue(32)
+        self.conn_argkw = conn_argkw
+        if 'host' in conn_argkw:
+            self.name = '{host}:{port}'.format(**conn_argkw)
+        else:
+            self.name = '{unix_socket}'.format(**conn_argkw)
+
+        self.stat = {'name': self.name,
+                     'create': 0,
+                     'pool_get': 0,
+                     'pool_put': 0,
+                     }
+
+    def get_conn(self):
+
+        try:
+            conn = self.queue.get(block=False)
+            self.stat['pool_get'] += 1
+            logger.debug('reuse connection:' + repr(self.stat))
+            return conn
+
+        except Queue.Empty:
+
+            conn = new_connection(self.conn_argkw, options=self.options)
+
+            self.stat['create'] += 1
+            logger.info('create new connection: ' + repr(self.stat))
+
+            return conn
+
+    def put_conn(self, conn):
+
+        try:
+            self.queue.put(conn, block=False)
+            self.stat['pool_put'] += 1
+            logger.debug('put connection:' + repr(self.stat))
+
+        except Queue.Full:
+            conn.close()
+
+
+class ConnectionWrapper(object):
+
+    def __init__(self, pool):
+        self.pool = pool
+        self.conn = None
+
+    def __enter__(self):
+        self.conn = self.pool.get_conn()
+        return self.conn
+
+    def __exit__(self, errtype, errval, _traceback):
+
+        if errtype is None:
+            self.pool.put_conn(self.conn)
+            self.conn = None
+        else:
+            self.conn.close()
+
+
+def make(conn_argkw, options=None):
+
+    pool = MysqlConnectionPool(conn_argkw, options=options)
+
+    def pool_api(action=None):
+
+        if action is None or action == 'get_conn':
+            return ConnectionWrapper(pool)
+        elif action == 'stat':
+            return pool.stat
+        else:
+            raise ValueError(action, 'invalid action: ' + repr(action))
+
+    pool_api.query = lambda *args, **kwargs: query(pool_api, *args, **kwargs)
+
+    return pool_api
+
+
+def conn_query(conn, sql, use_dict=True):
+
+    if use_dict:
+        cur = conn.cursor(MySQLdb.cursors.DictCursor)
+    else:
+        cur = conn.cursor()
+
+    cur.execute( sql )
+    rst = cur.fetchall()
+    cur.close()
+
+    return rst
+
+
+def query(pool, sql, use_dict=True):
+
+    with pool() as conn:
+        return conn_query(conn, sql, use_dict=use_dict)
+
+
+def new_connection(conn_argkw, conv=None, options=None):
+
+    # useful arg could be added in future.:
+    # conn_argkw.init_command
+
+    conv = conv or {}
+    options = options or {}
+
+    opt = {
+        'autocommit': 1,
+    }
+    opt.update(options)
+
+    conn = MySQLdb.connect(conv=conv, **conn_argkw)
+    for k, v in opt.items():
+        conn.query('set {k}={v}'.format(k=k, v=v))
+
+    return conn
---
+++ test/test_mysqlconnpool.py
@@ -0,0 +1,276 @@
+#!/usr/bin/env python2
+# coding: utf-8
+
+import time
+import socket
+import subprocess
+import unittest
+
+import docker
+
+import mysqlconnpool
+
+_DEBUG_ = True
+
+mysql_test_password = '123qwe'
+mysql_test_port = 3306
+mysql_test_user = 'root'
+mysql_test_name = 'mysql_test'
+mysql_test_tag = 'daocloud.io/mysql:5.7.13'
+
+
+class ShellError(Exception):
+    pass
+
+
+class Testmysqlconnpool(unittest.TestCase):
+
+    mysql_ip = None
+
+    def setUp(self):
+
+        self.mysql_ip = start_mysql_server()
+
+        addr = (self.mysql_ip, mysql_test_port)
+
+        # some time it takes several seconds to start listening
+        for ii in range(40):
+            try:
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                sock.connect(addr)
+                break
+            except socket.error:
+                dd('trying to connect to {0} failed'.format(str(addr)))
+                sock.close()
+                time.sleep(.4)
+        else:
+            raise
+
+        self.pool = mysqlconnpool.make({
+            'host': self.mysql_ip,
+            'port': mysql_test_port,
+            'user': mysql_test_user,
+            'passwd': mysql_test_password,
+        })
+
+    def tearDown(self):
+        stop_mysql_server()
+
+    def test_pool_name(self):
+
+        pool = self.pool
+
+        name = '{0}:{1}'.format(self.mysql_ip, mysql_test_port)
+
+        self.assertEqual(name, pool('stat')['name'])
+
+    def test_initial_stat(self):
+
+        pool = self.pool
+
+        dd('pool stat: {0}'.format(pool('stat')))
+
+        self.assertEqual(0, pool('stat')['create'])
+        self.assertEqual(0, pool('stat')['pool_get'])
+        self.assertEqual(0, pool('stat')['pool_put'])
+
+    def test_get_connection(self):
+
+        pool = self.pool
+
+        with pool() as conn:
+
+            dd(conn)
+            dd('pool stat: {0}'.format(pool('stat')))
+
+            self.assertEqual(0, pool('stat')['pool_put'])
+
+        dd('pool stat: {0}'.format(pool('stat')))
+
+        self.assertEqual(1, pool('stat')['create'])
+        self.assertEqual(0, pool('stat')['pool_get'])
+        self.assertEqual(1, pool('stat')['pool_put'])
+
+    def test_connection_is_working(self):
+
+        pool = self.pool
+
+        with pool() as conn:
+
+            dd(conn)
+
+            conn.query('show databases')
+            rst = conn.store_result()
+            rst = rst.fetch_row(rst.num_rows())
+
+            databases = [x[0] for x in rst]
+            self.assertTrue('mysql' in databases)
+
+    def test_get_connection_twice(self):
+
+        pool = self.pool
+
+        c0 = None
+        with pool() as conn:
+            c0 = conn
+
+        # the second time do not create new connection
+        with pool() as conn:
+
+            self.assertTrue(c0 is conn)
+
+            dd('pool stat: {0}'.format(pool('stat')))
+
+            self.assertEqual(1, pool('stat')['create'])
+            self.assertEqual(1, pool('stat')['pool_get'])
+
+        # after put back
+        dd('pool stat: {0}'.format(pool('stat')))
+
+        self.assertEqual(2, pool('stat')['pool_put'])
+
+    def test_raise_in_with(self):
+
+        pool = self.pool
+
+        try:
+            with pool() as conn:
+                dd(conn)
+                raise ValueError(1)
+        except ValueError:
+            pass
+
+        dd('pool stat: {0}'.format(pool('stat')))
+
+        self.assertEqual(1, pool('stat')['create'])
+        self.assertEqual(0, pool('stat')['pool_get'])
+        self.assertEqual(0, pool('stat')['pool_put'])
+
+
+    def test_conn_query(self):
+
+        pool = self.pool
+
+        with pool() as conn:
+            rst = mysqlconnpool.conn_query(conn, 'show databases')
+
+        dd('databases: {0}'.format(rst))
+
+        databases = [x['Database'] for x in rst]
+        self.assertTrue('mysql' in databases)
+
+
+    def test_query(self):
+
+        pool = self.pool
+
+        rst = pool.query('show databases')
+
+        dd('databases: {0}'.format(rst))
+
+        databases = [x['Database'] for x in rst]
+        self.assertTrue('mysql' in databases)
+
+
+    def test_query_array_result(self):
+
+        pool = self.pool
+
+        rst = pool.query('show databases', use_dict=False)
+
+        dd('databases: {0}'.format(rst))
+
+        databases = [x[0] for x in rst]
+        self.assertTrue('mysql' in databases)
+
+
+def start_mysql_server():
+
+    run_shell('service', 'docker', 'start')
+
+    if not docker_does_container_exist(mysql_test_name):
+
+        dd('create container: ' + mysql_test_name)
+        dcli = _docker_cli()
+        dcli.create_container(name=mysql_test_name,
+                             environment={
+                                 'MYSQL_ROOT_PASSWORD': mysql_test_password,
+                             },
+                             image=mysql_test_tag,
+                             )
+        time.sleep(2)
+
+    dd('start mysql: ' + mysql_test_name)
+    dcli = _docker_cli()
+    dcli.start(container=mysql_test_name)
+
+    dd('get mysql ip inside container')
+    rc, out, err = docker_cmd(
+        'run',
+        '-it',
+        '--link', mysql_test_name + ':mysql',
+        '--rm', mysql_test_tag,
+        'sh', '-c', 'exec echo "$MYSQL_PORT_3306_TCP_ADDR"',
+    )
+
+    ip = out.strip()
+    dd('ip: ' + repr(ip))
+
+    return ip
+
+
+def stop_mysql_server():
+
+    dcli = _docker_cli()
+    dcli.stop(container=mysql_test_name)
+
+
+def docker_does_container_exist(name):
+
+    dcli = _docker_cli()
+    try:
+        dcli.inspect_container(name)
+        return True
+    except docker.errors.NotFound:
+        return False
+
+
+def _docker_cli():
+    dcli = docker.Client(base_url='unix://var/run/docker.sock')
+    return dcli
+
+
+def docker_cmd(*args, **argkv):
+    args = ['docker'] + list(args)
+    return run_shell(*args, **argkv)
+
+
+def run_shell(*args, **argkv):
+
+    subproc = subprocess.Popen(args,
+                               close_fds=True,
+                               cwd=None,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE, )
+
+    out, err = subproc.communicate()
+
+    subproc.wait()
+
+    if subproc.returncode != 0:
+        raise ShellError(subproc.returncode, out, err, args, argkv)
+
+    rst = [subproc.returncode, out, err]
+
+    return rst
+
+
+
+def dd( *msg ):
+    if not _DEBUG_:
+        return
+
+    for m in msg:
+        print str( m ),
+    print
