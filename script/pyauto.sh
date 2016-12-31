2f59ed0c7b4c18c7c942fc028443574e2e8a1460
drdr xp <drdr.xp@gmail.com>
2016-12-31 21:00:33 +0800

    pyauto.sh: add pyflakes check. accept $1 as sub dir to check

--- script/pyauto.sh
+++ script/pyauto.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-autopep8  -i  $(find . -name "*.py")
-autoflake -i  $(find . -name "*.py")
-isort         $(find . -name "*.py")
+path="${1-.}"
+
+find "$path" -name "*.py" | while read fn; do
+    pyflakes      "$fn"
+    autopep8  -i  "$fn"
+    autoflake -i  "$fn"
+    isort         "$fn"
+done
