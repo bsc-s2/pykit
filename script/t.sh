71f123a46daa071592fe747b3da6ffe54bc49d7a
drdr xp <drdr.xp@gmail.com>
2016-12-31 20:32:55 +0800

    t.sh support testing a single function

--- script/t.sh
+++ script/t.sh
@@ -3,5 +3,11 @@
 if [ $# -eq 0 ]; then
     python2 -m unittest discover -v --failfast
 else
-    python2 -m unittest discover -v --failfast -s "$1"
+    if [ -d "$1" ]; then
+        # find test from a subdir
+        python2 -m unittest discover -v --failfast -s "$1"
+    else
+        # find test by module[.Class[.function]]
+        python2 -m unittest -v --failfast "$1"
+    fi
 fi
