6475ad075ebc7f52c9988aaf0329d1465c900a65
liubaohai <617708924@qq.com>
2017-04-07 15:07:29 +0800

    add cacheable module

---
+++ cacheable/cacheable.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python2
+# coding: utf-8
+
+import copy
+import logging
+import threading
+import time
+
+logger = logging.getLogger(__name__)
+
+
+class LRU(object):
+
+    def __init__(self, capacity, timeout=60):
+
+        self.lock = threading.RLock()
+        self.capacity = capacity
+        self.cleanup_threshold = int(capacity * 1.5)
+        self.timeout = timeout
+        self.item_old_time = int(timeout / 2)
+        self.size = 0
+        self.items = {}
+        self.head = {'next': None, 'pre': None}
+        self.tail = self.head
+
+    def __getitem__(self, key):
+
+        now = int(time.time())
+
+        with self.lock:
+            item = self.items[key]
+
+            if now > item['tm'] + self.timeout:
+                self._del_item(item)
+                raise KeyError
+
+            self._move_to_tail(item)
+
+            return (item['obj'], (now > item['tm'] + self.item_old_time))
+
+    def __setitem__(self, key, val):
+
+        with self.lock:
+            if self.items.has_key(key):
+                item = self.items[key]
+                item['obj'] = val
+                item['tm'] = int(time.time())
+
+                self._move_to_tail(item)
+
+            else:
+                self.items[key] = {'key': key,
+                                   'obj': val,
+                                   'pre': None,
+                                   'next': None,
+                                   'tm': int(time.time())}
+
+                self._move_to_tail(self.items[key])
+
+                self.size += 1
+
+                if self.size > self.cleanup_threshold:
+                    self._cleanup()
+
+    def _remove_item(self, item):
+
+        item['pre']['next'] = item['next']
+        if item['next'] is not None:
+            item['next']['pre'] = item['pre']
+        else:
+            self.tail = item['pre']
+
+    def _move_to_tail(self, item):
+
+        with self.lock:
+            if item['pre'] is not None:
+                self._remove_item(item)
+
+            self.tail['next'] = item
+            item['pre'] = self.tail
+            item['next'] = None
+            self.tail = item
+
+    def _del_item(self, item):
+
+        with self.lock:
+            del self.items[item['key']]
+            self._remove_item(item)
+            self.size -= 1
+
+    def _cleanup(self):
+
+        with self.lock:
+            while self.size > self.capacity:
+                item = self.head['next']
+                self._del_item(item)
+
+
+class Cacheable(object):
+
+    cachers = {}
+
+    def __init__(self, capacity=1024 * 4, timeout=60, is_deepcopy=True, key_extractor=None):
+
+        self.lru_obj = LRU(capacity, timeout)
+        self.is_deepcopy = is_deepcopy
+        self.key_extractor = key_extractor or Cacheable.arg_str
+        self.name = '-no-name-'
+        self.cache_hit = 0
+        self.cache_missed = 0
+
+    @classmethod
+    def cache(clz, name, capacity=1024 * 4, timeout=60, is_deepcopy=True, key_extractor=None):
+
+        cacher = clz.cachers.get(name, clz(capacity, timeout,
+                                      is_deepcopy, key_extractor))
+        clz.cachers[name] = cacher
+        cacher.name = name
+
+        return cacher.cache_wrapper
+
+    @staticmethod
+    def arg_str(args, argkv):
+
+        argkv = [(k, v) for [k, v] in argkv.items()]
+        argkv.sort()
+
+        return str([args, argkv])
+
+
+class ProcessWiseCache(Cacheable):
+
+    def cache_wrapper(cself, fun):
+
+        def func_wrapper(*args, **argkv):
+
+            arg_str = None
+
+            try:
+                arg_str = cself.key_extractor(args, argkv)
+            except Exception as e:
+                arg_str = Cacheable.arg_str(args, argkv)
+                logger.exception(repr(e) + ' while generate LRU item key')
+
+            val = None
+
+            try:
+                (val, is_old) = cself.lru_obj[arg_str]
+                cself.cache_hit += 1
+            except KeyError as e:
+                val = fun(*args, **argkv)
+                cself.lru_obj[arg_str] = val
+                cself.cache_missed += 1
+
+                logger.info(repr(e)
+                            + ' while getitem from LRU, the  key: {s}'.format(s=arg_str))
+            except Exception as e:
+                logger.exception(repr(e)
+                                 + ' while getitem from LRU, the key: {s}'.format(s=arg_str))
+                raise
+
+            if cself.is_deepcopy:
+                return copy.deepcopy(val)
+            else:
+                return val
+
+        return func_wrapper
---
+++ cacheable/test/test_cacheable.py
@@ -0,0 +1,277 @@
+#!/usr/bin/env python2
+# coding: utf-8
+
+import time
+import unittest
+
+from pykit import cacheable
+
+
+class TestLRU(unittest.TestCase):
+
+    def _assure_lru_list(self, lru_obj):
+
+        size = lru_obj.size
+        item_head = lru_obj.head
+        item_tail = lru_obj.tail
+        for i in xrange(size):
+            item_head = item_head['next']
+            item_tail = item_tail['pre']
+
+        self.assertTrue(item_head == lru_obj.tail and item_tail == lru_obj.head)
+
+    def _assure_lru_items_order(self, lru_obj, order_keys):
+
+        item = lru_obj.head['next']
+        lru_keys = []
+        while item is not None:
+            lru_keys.append(item['key'])
+            item = item['next']
+
+        self.assertTrue(order_keys == lru_keys)
+
+    def test_lru_timeout(self):
+
+        cases = (
+            ('k1', 'v1', 5, True, True),
+            ('k2', 'v2', 4, True, False),
+            ('k3', 'v3', 1, False, False),
+        )
+
+        lru_obj = cacheable.LRU(10, 4)
+        for key, val, sleep_time, is_old, is_timeout in cases:
+            lru_obj[key] = val
+            time.sleep(sleep_time)
+
+            try:
+                (_, old) = lru_obj[key]
+                self.assertEqual(old, is_old)
+                self.assertFalse(is_timeout)
+
+            except KeyError:
+                self._assure_lru_list(lru_obj)
+                self.assertTrue(is_timeout)
+
+    def test_lru_getitem(self):
+
+        capacity = 4
+        cases = (
+            (3,
+             [0, 1, 2],
+             []),
+
+            (4,
+             [0, 1, 2, 3],
+             []),
+
+            (5,
+             [0, 1, 2, 3, 4],
+             []),
+
+            (6,
+             [0, 1, 2, 3, 4, 5],
+             []),
+
+            (7,
+             [3, 4, 5, 6],
+             [0, 1, 2]),
+        )
+
+        for insert_count, exist_items, cleanup_items in cases:
+            lru_obj = cacheable.LRU(capacity, 10)
+            for i in xrange(insert_count):
+                lru_obj[i] = 'val%d' % (i)
+
+            for i in xrange(insert_count):
+                try:
+                    (item, _) = lru_obj[i]
+                    self.assertEqual(item, 'val%d' % (i))
+                    self.assertEqual(lru_obj.tail['key'], i)
+                    self.assertTrue(i in exist_items)
+
+                except KeyError:
+                    self.assertTrue(i in cleanup_items)
+
+    def test_lru_setitem(self):
+
+        capacity = 4
+        cases = (
+            (3,
+             [0, 1, 2]),
+
+            (4,
+             [0, 1, 2, 3]),
+
+            (5,
+             [0, 1, 2, 3, 4]),
+
+            (6,
+             [0, 1, 2, 3, 4, 5]),
+
+            (7,
+             [3, 4, 5, 6]),
+        )
+
+        for insert_count, expect_order_keys in cases:
+            lru_obj = cacheable.LRU(capacity, 10)
+            for i in xrange(insert_count):
+                lru_obj[i] = 'val'
+                self._assure_lru_list(lru_obj)
+
+            self._assure_lru_items_order(lru_obj, expect_order_keys)
+
+    def test_lru_capacity(self):
+
+        cases = (
+                (0,
+                 ((0, 0), (1, 0))),
+
+                (1,
+                 ((1, 1), (2, 1))),
+
+                (10,
+                 ((9, 9), (10, 10), (13, 13), (15, 15), (16, 10))),
+        )
+
+        for capacity, case in cases:
+            for insert_count, expect_size in case:
+                lru_obj = cacheable.LRU(capacity, 60)
+                for i in xrange(insert_count):
+                    lru_obj[i] = 'val'
+
+                self.assertEqual(lru_obj.size, expect_size)
+
+
+class TestProcessWiseCache(unittest.TestCase):
+
+    @cacheable.ProcessWiseCache.cache('method_cache_data', capacity=10, timeout=4, is_deepcopy=False)
+    def _method_cache_data(self, key):
+        data = need_cache_data.get(key, {})
+        data['tm'] = time.time()
+        return data
+
+    def test_get_items_from_cache(self):
+
+        cache_items = {}
+        for key in need_cache_data.iterkeys():
+            cache_items[key] = get_cache_data(key)
+
+        time.sleep(0.1)
+        for key in need_cache_data.iterkeys():
+            self.assertEqual(get_cache_data(key), cache_items[key])
+
+    def test_get_items_from_cache_use_method(self):
+
+        cache_items = {}
+        for key in need_cache_data.iterkeys():
+            cache_items[key] = self._method_cache_data(key)
+
+        time.sleep(0.1)
+        for key in need_cache_data.iterkeys():
+            self.assertEqual(self._method_cache_data(key), cache_items[key])
+
+    def test_cache_item_timeout_and_cache_again(self):
+
+        cases = (
+            (1, False),
+            (2, False),
+            (2, True),
+        )
+
+        tm = get_cache_data('key')['tm']
+        for sleep_time, is_timeout in cases:
+            time.sleep(sleep_time)
+            if is_timeout:
+                self.assertNotEqual(tm, get_cache_data('key')['tm'])
+            else:
+                self.assertEqual(tm, get_cache_data('key')['tm'])
+
+    def test_get_deepcopy_item_from_cache(self):
+
+        data = get_deepcopy_of_cache_data('key1')
+        data['tm'] = 10000
+        self.assertNotEqual(get_deepcopy_of_cache_data('key1'), data)
+
+    def test_default_key_extractor(self):
+
+        cases = (
+            ((),
+             {},
+             '[(), []]'),
+
+            ((1),
+             {1: 'val_1'},
+             '[1, [(1, \'val_1\')]]'),
+
+            ((1),
+             {'a': 'val_a'},
+             '[1, [(\'a\', \'val_a\')]]'),
+
+            (('c'),
+             {'b': 'val_b'},
+             '[\'c\', [(\'b\', \'val_b\')]]'),
+
+            ((1, 'c'),
+             {'a': 'val_a', 'b': 'val_b'},
+             '[(1, \'c\'), [(\'a\', \'val_a\'), (\'b\', \'val_b\')]]'),
+
+            (('c', 1),
+             {'b': 'val_b', 'a': 'val_a'},
+             '[(\'c\', 1), [(\'a\', \'val_a\'), (\'b\', \'val_b\')]]'),
+        )
+
+        for args, argkv, expect_str in cases:
+            s = cacheable.ProcessWiseCache.arg_str(args, argkv)
+            self.assertEqual(s, expect_str)
+
+    def test_define_key_extractor(self):
+
+        cases = (
+            ('valueerror',
+             cacheable.ProcessWiseCache.arg_str(('valueerror', ), {})),
+
+            ('typeerror',
+             cacheable.ProcessWiseCache.arg_str(('typeerror', ), {})),
+
+            ('key',
+             key_extractor_funtion(('key', ), {}))
+        )
+
+        lru_obj = cacheable.ProcessWiseCache.cachers['cache_data'].lru_obj
+        for key, expect_generate_key in cases:
+            get_cache_data(key)
+            self.assertTrue(lru_obj.items.has_key(expect_generate_key))
+
+
+def key_extractor_funtion(args, argkv):
+
+    if 'valueerror' in args:
+        raise ValueError
+
+    if 'typeerror' in args:
+        raise TypeError
+
+    return str(args) + str(argkv)
+
+
+@cacheable.ProcessWiseCache.cache('deepcopy_of_cache_data', capacity=100, timeout=60, is_deepcopy=True)
+def get_deepcopy_of_cache_data(key):
+
+    return need_cache_data.get(key, {})
+
+
+@cacheable.ProcessWiseCache.cache('cache_data', capacity=100, timeout=4,
+                                  is_deepcopy=False, key_extractor=key_extractor_funtion)
+def get_cache_data(key):
+
+    cache_data = need_cache_data.get(key, {})
+    cache_data['tm'] = time.time()
+    return cache_data
+
+
+need_cache_data = {
+    'key1': {'tm': 0},
+    'key2': {'tm': 0},
+    'key3': {'tm': 0},
+    'key4': {'tm': 0},
+}
