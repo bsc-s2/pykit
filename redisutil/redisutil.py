621e7cd16534353f2f4a744bbfee03203a41f0ec
drdr xp <drdr.xp@gmail.com>
2017-07-07 20:32:31 +0800

    M  redisutil/redisutil.py

--- redisutil/redisutil.py
+++ redisutil/redisutil.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 # redis is thread safe
 
 # NOTE: fork may duplicate file descriptor that confuse connection pool.
-_pid_client = (0, None)
+_pid_client = dict(pid=0, client=None)
 _lock = threading.RLock()
 
 
@@ -18,12 +18,14 @@ def get_client(ip_port):
     if isinstance(ip_port, (int, long)):
         ip_port = ('127.0.0.1', ip_port)
 
+    return _pid_client['client']
+
     pid = os.getpid()
 
-    if _pid_client[0] != pid:
+    if _pid_client['pid'] != pid:
         with _lock:
-            if _pid_client[0] != pid:
+            if _pid_client['pid'] != pid:
                 logger.info('create redis client for process-id: ' + repr(pid))
-                _pid_client = (pid, redis.StrictRedis(*ip_port))
+                _pid_client.update(dict(pid=pid, client=redis.StrictRedis(*ip_port)))
 
-    return _pid_client[0]
+    return _pid_client['client']
--- redisutil/test/test_redisutil.py
+++ redisutil/test/test_redisutil.py
@@ -1,6 +1,8 @@
 import os
+import sys
 import subprocess
 import time
+import redis
 import unittest
 
 from pykit import redisutil
@@ -26,56 +28,117 @@ def read_file(fn):
 
 class TestRedis(unittest.TestCase):
 
-    foo_fn = '/tmp/foo'
-    bar_fn = '/tmp/bar'
-    pidfn = '/tmp/test_daemonize.pid'
-
-    def _clean(self):
-
-        # kill foo.py and kill bar.py
-        # bar.py might be waiting for foo.py to release lock-file.
-        try:
-            subproc('python2 {b}/foo.py stop'.format(b=this_base))
-        except Exception as e:
-            print repr(e)
-
-        time.sleep(0.1)
-
-        try:
-            subproc('python2 {b}/bar.py stop'.format(b=this_base))
-        except Exception as e:
-            print repr(e)
-
-        # remove written file
-
-        try:
-            os.unlink(self.foo_fn)
-        except EnvironmentError as e:
-            pass
-
-        try:
-            os.unlink(self.bar_fn)
-        except EnvironmentError as e:
-            pass
-
     def setUp(self):
-        self.redis_name = 'redis-0'
-        self.ip = '192.168.52.40'
-        utdocker.create_network()
-        utdocker.start_container(self.redis_name, redis_tag, self.ip, '')
 
-        self.rcl = redisutil.get_client((self.ip, 6379))
-        # self._clean()
+        self.docker_name = 'redis-0'
+        self.ip = '192.168.52.40'
+        self.port = 6379
+        self.is_child = False
+
+        utdocker.create_network()
+        utdocker.start_container(self.docker_name, redis_tag, self.ip, '')
+        dd('started redis in docker')
+
+        # wait for redis to work
+
+        t = time.time() + 5
+        while time.time() < t:
+            self.rcl = redisutil.get_client((self.ip, self.port))
+            try:
+                self.rcl.hget('foo', 'foo')
+                dd('redis is ready: ' + repr((self.ip, self.port)))
+                break
+            except redis.ConnectionError as e:
+                dd('can not connect to redis: ' + repr((self.ip, self.port)))
+                time.sleep(0.1)
+                continue
+        # os.system('docker ps')
 
     def tearDown(self):
-        pass
-        # self._clean()
 
-    def test_start(self):
+        # do not tear down in child process
+        if self.is_child:
+            return
+
+        dd('remove_container: ' + self.docker_name)
+        utdocker.remove_container(self.docker_name)
+
+    def test_set_get(self):
 
         hname = 'foo'
-        rst = self.rcl.hset(hname, 'a', 1)
+
+        rst = self.rcl.hset(hname, 'a', '1')
         dd('hset rst:', rst)
 
         rst = self.rcl.hget(hname, 'a')
         dd('hget rst:', rst)
+
+        self.assertEqual('1', rst)
+
+    def test_recreate_client(self):
+
+        hname = 'foo'
+
+        pid = os.fork()
+
+        dd('my pid:', os.getpid())
+
+        if pid == 0:
+            # child
+            self.is_child = True
+            rcl = redisutil.get_client((self.ip, self.port))
+            rcl.hset(hname, 'a', '5')
+
+            child_exit()
+
+        else:
+            # parent
+
+            os.waitpid(pid, 0)
+            dd('child pid quit: ' + repr(pid))
+
+            rst = self.rcl.hget(hname, 'a')
+
+            dd('redis hget rst:', rst)
+
+            self.assertEqual('5', rst)
+
+    def test_separated_connection_after_fork(self):
+
+        hname = 'foo'
+
+        pid = os.fork()
+        dd('my pid:', os.getpid())
+        n = 10240
+
+        if pid == 0:
+            # child
+            self.rcl = redisutil.get_client((self.ip, self.port))
+
+        # for both parent and child process
+        for i in range(n):
+            self.rcl.hset(hname, 'x' + str(i), 'foobarjfkdslafjdasklfdjsaklfdsa' + str(i))
+            self.rcl.hget(hname, 'x' + str(i-1))
+
+            if i % 100 == 0:
+                dd(os.getpid(), ' finished ', i, ' set/get')
+
+        if pid == 0:
+            # child
+            self.is_child = True
+            child_exit()
+        else:
+            # parent
+            os.waitpid(pid, 0)
+            dd('child pid quit: ' + repr(pid))
+
+        # no error raised with 2 process hget/hset, it is ok
+
+
+def child_exit():
+    # to suppress error message about sys.exit
+    os.close(0)
+    os.close(1)
+    os.close(2)
+
+    sys.exit(0)
