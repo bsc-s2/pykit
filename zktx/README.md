c1a47f5a478c5f63e657a29a08869c53c04a60d8
drdr xp <drdr.xp@gmail.com>
2018-05-14 17:29:17 +0800

    zktx: add status and storage layer abstraction and helper functions

--- zktx/README.md
+++ zktx/README.md
@@ -29,7 +29,8 @@ def get(self, key):
 ```
 
 They are similar to zk APIs, except that `version` does not have to be a version
-number. It could be any data type the implementation could understand.
+number.
+It could be any data type the implementation could understand.
 
 
 ##  zktx.ValueAccessor
@@ -44,6 +45,201 @@ def delete(self, version=None):
 def set(self, value, version=None):
 def get(self):
 ```
+
+
+##  zktx.TXStorage
+
+**syntax**:
+`zktx.TXStorage()`
+
+This is an abstract class that defines what a storage layer should provides for
+a transaction engine.
+
+Our TX engine is able to run on any storage that implements `TXStorage`.
+
+
+### TXStorage attributes
+
+To support a transaction to run,
+a class that implements `TXStorage` must provides 3 accessors(`KVAccessor` and `ValueAccessor`):
+
+-   `record`:
+    is a `KVAccessor` to get or set a user-data record.
+
+    A record value is a `dict` map of `txid` to value:
+
+    ```python
+    {
+        <txid>: <value>
+        <txid>: <value>
+        ...
+    }
+    ```
+
+-   `journal`:
+    is a `KVAccessor` to get or set a tx journal.
+
+    Journal value is not define on storage layer.
+    A TX engine defines the value format itself.
+
+-   `txidset`:
+    is a `ValueAccessor`.
+    It is a single value accessor to get or set transaction id set.
+
+    Value of `txidset` is a `dict` of 3 `RangeSet`(see module `rangeset`):
+
+    ```python
+    {
+        "COMMITTED": RangeSet(),
+        "ABORTED": RangeSet(),
+        "PURGED": RangeSet(),
+    }
+    ```
+
+    -   `COMMITTED` contains committed txid.
+
+    -   `ABORTED` contains aborted txid.
+        Abort means a tx is killed before writing a `journal`.
+
+    -   `PURGED` contains txid whose journal has been removed.
+
+    > `COMMITTED`, `ABORTED` and `PURGED` has no intersection.
+
+
+### TXStorage methods
+
+An implementation of `TXStorage` must implement 2 locking methods:
+
+####  TXStorage.try_lock_key
+
+**syntax**:
+`TXStorage.try_lock_key(txid, key)`
+
+It is defined as `def try_lock_key(self, txid, key)`.
+
+It tries to lock a `key` for a `txid`: Same `txid` can lock a `key` more than once.
+
+This function should never block and should return at once.
+
+> Because our TX engine need to detect and resolve deadlock, thus locking should
+> be non-blocking.
+
+It should return a 3 element `tuple`:
+
+-   A `bool` indicates if locking succeeds.
+
+-   A `txid` indicates current lock holder.
+    If locking succeeded, it is the passed in `txid`
+
+-   A 3rd value indicates lock stat(not used yet).
+
+
+####  TXStorage.try_release_key
+
+**syntax**:
+`TXStorage.try_release_key(txid, key)`
+
+It should release the lock identified by `key`, if and only if the lock is held
+by `txid`
+
+> This way only the lock holder could release the lock, without the need to
+> know if it has already acquired the lock.
+> This makes the recovery of a tx processor very easy:
+> A recovered process only need to know the `txid` but not the locking
+> informations.
+
+It should returns 3 element `tuple`:
+
+-   A `bool` indicates if the lock is previously locked by any one.
+
+-   A `txid` indicates current lock holder.
+    If no one has been locking it, it is the passed in `txid`
+
+-   A 3rd value indicates lock stat(not used yet).
+
+### TXStorage helper methods
+
+There are also two 3 methods an TX engine requires, which are already provided
+by `TXStorageHelper`.
+
+An implementation class could just extend `TXStorageHelper` to make these 3 methods available.
+See `TXStorageHelper`.
+
+
+##  zktx.TXStorageHelper
+
+**syntax**:
+`class TXStorageHelper(object)`
+
+It provides 3 methods those a TX engine relies on.
+Since underlying accessors has already been provided, these 3 methods are
+implementation unrelated.
+
+
+###  TXStorageHelper.get_latest
+
+**syntax**:
+`TXStorageHelper.get_latest(key)`
+
+It returns the latest update(the update with the greatest txid) of a record identified by `key`.
+
+It requires 1 accessor method: `self.record.get(key)`.
+
+**arguments**:
+
+-   `key`:
+    specifies the `key` of the record.
+
+**return**:
+a dict in form of `{"txid": ..., "value": ...}` and a implementation defined version.
+
+
+###  TXStorageHelper.apply_record
+
+**syntax**:
+`TXStorageHelper.apply_record(txid, key, value)`
+
+This method applies an update to underlying storage.
+
+It requires 2 accessor methods: `self.record.get(key)`
+and `self.record.set(key, value, version=None)`.
+
+**arguments**:
+
+-   `txid`:
+    transaction id.
+
+-   `key`:
+    record key.
+
+-   `value`:
+    record value.
+
+**return**:
+a `bool` indicates if change has been made to underlying storage.
+Normal it is `False` if a higher txid has already applied.
+
+
+###  TXStorageHelper.add_to_txidset
+
+**syntax**:
+`TXStorageHelper.add_to_txidset(status, txid)`
+
+It records a txid as one of the possible status: COMMITTED, ABORTED or PURGED.
+
+It requires 2 accessor methods: `self.txidset.get()`
+and `self.txidset.set(value, version=None)`.
+
+**arguments**:
+
+-   `status`:
+    specifies tx status
+
+-   `txid`:
+    transaction id.
+
+**return**:
+Nothing
 
 
 #   Author
---
+++ zktx/test/test_txstorage.py
@@ -0,0 +1,214 @@
+import threading
+import unittest
+
+from kazoo.exceptions import BadVersionError
+
+from pykit import rangeset
+from pykit import threadutil
+from pykit import ututil
+from pykit import zktx
+from pykit.zktx import COMMITTED
+from pykit.zktx import STATUS
+
+dd = ututil.dd
+
+zk_tag = 'daocloud.io/zookeeper:3.4.10'
+zk_name = 'zk_test'
+
+
+class PseudoKVAccessor(dict):
+
+    def __init__(self, *args, **kwargs):
+
+        super(PseudoKVAccessor, self).__init__(*args, **kwargs)
+
+        self.lock = threading.RLock()
+
+    def get(self, key):
+
+        with self.lock:
+            return super(PseudoKVAccessor, self).get(key)
+
+    def set(self, key, value, version):
+
+        with self.lock:
+
+            prev = self[key]
+            if prev[1] != version:
+                raise BadVersionError()
+
+            self[key] = (value, version+1)
+
+
+class TxidsetAccessor(object):
+    def __init__(self):
+        self.lock = threading.RLock()
+        self.ver = 0
+        self.d = {}
+
+    def get(self):
+        with self.lock:
+            for k in self.d:
+                self.d[k] = rangeset.RangeSet(self.d[k])
+
+            return self.d, self.ver
+
+    def set(self, value, version=None):
+        with self.lock:
+            self.d = value
+            self.version = version + 1
+
+
+class PseudoStorage(zktx.TXStorageHelper):
+
+    def __init__(self):
+        self.record = PseudoKVAccessor({
+            'foo': ({k: k for k in (1, 17)}, 0),
+            'bar': ({-1: None}, 1),
+        })
+
+        self.txidset = TxidsetAccessor()
+
+
+class TestTXStorageHelper(unittest.TestCase):
+
+    def setUp(self):
+        self.sto = PseudoStorage()
+
+    def test_get_latest(self):
+
+        # get_latest relies on storage.record.get
+
+        rst = self.sto.get_latest('foo')
+        self.assertEqual(({'txid': 17, 'value': 17}, 0), rst)
+
+        rst = self.sto.get_latest('bar')
+        self.assertEqual(({'txid': -1, 'value': None}, 1), rst)
+
+    def test_apply_record(self):
+
+        cases = (
+                (0,  'foo', 'fooval', (None,     0), '<=max txid'),
+                (1,  'foo', 'fooval', (1,        0), '<=max txid'),
+                (16, 'foo', 'fooval', (None,     0), '<=max txid'),
+                (17, 'foo', 'fooval', (17,       0), '<=max txid'),
+                (18, 'foo', 'fooval', ('fooval', 1), '>max txid'),
+                (1,  'bar', 'barval', ('barval', 2), '>max txid'),
+                (1,  'bar', 'barval', ('barval', 2), '<=max txid'),
+        )
+
+        for txid, key, value, expected, mes in cases:
+
+            dd(txid, key, value, expected, mes)
+
+            self.sto.apply_record(txid, key, value)
+
+            rec, ver = self.sto.record.get(key)
+            self.assertEqual(expected, (rec.get(txid), ver))
+
+    def test_apply_record_max_history(self):
+
+        s, e = 100, 150
+        for txid in range(s, e):
+            self.sto.apply_record(txid, 'foo', txid)
+
+        rec, ver = self.sto.record.get('foo')
+        self.assertEqual(e-s, ver)
+        self.assertEqual(self.sto.max_value_history, len(rec))
+
+    def test_apply_record_concurrently(self):
+
+        n_thread = 10
+        n_repeat = 100
+
+        self.sto.max_value_history = n_thread * n_repeat + 1
+
+        l = threading.RLock()
+        sess = {'txid': 0}
+        success_tx = {-1: True}
+
+        def _apply(ithread):
+            for ii in range(n_repeat):
+
+                with l:
+                    sess['txid'] += 1
+                    txid = sess['txid']
+
+                if self.sto.apply_record(txid, 'bar', txid):
+                    with l:
+                        success_tx[txid] = True
+
+        ths = [threadutil.start_daemon(_apply, args=(ii, ))
+               for ii in range(n_thread)]
+
+        for th in ths:
+            th.join()
+
+        rec, ver = self.sto.record.get('bar')
+        dd(len(rec))
+        dd(ver)
+
+        self.assertEqual(set(success_tx.keys()),
+                         set(rec.keys()))
+
+        latest, ver = self.sto.get_latest('bar')
+        dd(latest, ver)
+
+        self.assertLessEqual(ver, 1+n_thread*n_repeat)
+        self.assertEqual(n_thread*n_repeat, latest['txid'])
+        self.assertEqual(n_thread*n_repeat, latest['value'])
+
+    def _rand_txids(self):
+        for ii in range(100):
+            txid = int(ii % 53 * 1.3)
+            yield txid
+
+    def test_add_to_txidset(self):
+
+        self.assertRaises(KeyError, self.sto.add_to_txidset, 'foo', 1)
+
+        expected = {txid: True
+                    for txid in self._rand_txids()}
+
+        for status in STATUS:
+            for txid in self._rand_txids():
+                self.sto.add_to_txidset(status, txid)
+
+            dd(sorted(expected.keys()))
+
+            rst, ver = self.sto.txidset.get()
+            dd(rst[status])
+
+            for txid in expected:
+                self.assertTrue(rst[status].has(txid))
+
+            for txid in range(100):
+                if txid not in expected:
+                    self.assertFalse(rst[status].has(txid))
+
+    def test_add_to_txidset_concurrent(self):
+
+        expected = {txid: True
+                    for txid in self._rand_txids()}
+
+        n_thread = 2
+        status = COMMITTED
+
+        def _add():
+            for txid in self._rand_txids():
+                self.sto.add_to_txidset(status, txid)
+
+        for th in [threadutil.start_daemon(_add)
+                   for _ in range(n_thread)]:
+
+            th.join()
+
+        rst, ver = self.sto.txidset.get()
+        dd(rst[status])
+
+        for txid in expected:
+            self.assertTrue(rst[status].has(txid))
+
+        for txid in range(100):
+            if txid not in expected:
+                self.assertFalse(rst[status].has(txid))
