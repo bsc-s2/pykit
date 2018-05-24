e2cb0e7c21ac6d638661315a4d0d20f869552546
drdr xp <drdr.xp@gmail.com>
2018-05-24 16:42:21 +0800

    abstract accessor add set_or_create method

--- zktx/accessor.py
+++ zktx/accessor.py
@@ -4,21 +4,25 @@
 
 class KeyValue(object):
 
-    def create(self, key, value): raise TypeError('unimplemented')
+    def create(self, key, value, ephemeral=False, sequence=False): raise TypeError('unimplemented')
 
     def delete(self, key, version=None): raise TypeError('unimplemented')
 
     def set(self, key, value, version=None): raise TypeError('unimplemented')
+
+    def set_or_create(self, key, value, version=None): raise TypeError('unimplemented')
 
     def get(self, key): raise TypeError('unimplemented')
 
 
 class Value(object):
 
-    def create(self, value): raise TypeError('unimplemented')
+    def create(self, value, ephemeral=False, sequence=False): raise TypeError('unimplemented')
 
     def delete(self, version=None): raise TypeError('unimplemented')
 
     def set(self, value, version=None): raise TypeError('unimplemented')
+
+    def set_or_create(self, value, version=None): raise TypeError('unimplemented')
 
     def get(self): raise TypeError('unimplemented')
