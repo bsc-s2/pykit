92ed659d6ade753a56011c8be45c939eaf6291bf
hubiyong <450139682@qq.com>
2018-09-20 11:44:42 +0800

    fix code style and exception dealing

--- request/request.py
+++ request/request.py
@@ -23,13 +23,16 @@ class InvalidMethodCall(RequestError):
     pass
 
 
+# the arg  must be str or unicode type
 def _basestring(arg=''):
-    if isinstance(arg, basestring) is False:
-        raise InvalidArgumentError('The argument must be str or unicode type')
+    if not isinstance(arg, basestring):
+        raise InvalidArgumentError('type of arg {x} is not str or unicode'.format(x=type(arg)))
+
     return arg
 
 
 class Request(FixedKeysDict):
+
     keys_default = dict((('verb', _basestring),
                          ('uri', _basestring),
                          ('args', dict),
@@ -39,18 +42,21 @@ class Request(FixedKeysDict):
                          ('do_add_auth', bool),))
 
     def __init__(self, *args, **argkv):
-
-        # content represents str or a file to upload in POST request
+        # content represents str or a file to upload in post request
         self.content = None
         super(FixedKeysDict, self).__init__(*args, **argkv)
 
         if self['verb'] == 'POST':
-            if self['fields'] == {} or self['body'] != '':
-                raise InvalidRequestError('In post request, dict needs fields attribute and body is made by fields')
-            if self['do_add_auth'] is False:
+            if len(self['fields']) == 0:
+                raise InvalidRequestError('fields is null when init post request')
+            if self['body'] != '':
+                raise InvalidRequestError('body is not null when init post request')
+            if not self['do_add_auth']:
                 self['body'], self['headers'] = self._make_post_body_headers()
-        elif self['fields'] != {}:
-                raise InvalidRequestError('non POST request must not contain fields attribute')
+
+        else:
+            if self['fields']:
+                raise InvalidRequestError('non-post request contains fields attribute: {fields}'.format(fields=self['fields']))
 
     def aws_sign(self, access_key, secret_key, query_auth=False, sign_payload=False, headers_not_to_sign=None,
                  request_date=None, signing_date=None, region='us-east-1', service='s3', expires=60):
@@ -58,19 +64,20 @@ class Request(FixedKeysDict):
         if headers_not_to_sign is None:
             headers_not_to_sign = []
 
-        if self['do_add_auth'] is False:
-            raise InvalidMethodCall('only add_auth request can call this method')
+        if not self['do_add_auth']:
+            raise InvalidMethodCall('non add_auth request calls aws_sign() method')
 
         signer = awssign.Signer(access_key, secret_key, region=region, service=service, default_expires=expires)
 
         if self['verb'] == 'POST':
-            res = signer.add_post_auth(self['fields'], request_date=request_date, signing_date=signing_date)
+            ctx = signer.add_post_auth(self['fields'], request_date=request_date, signing_date=signing_date)
             self['body'], self['headers'] = self._make_post_body_headers()
-            return res
+
         else:
-            res = signer.add_auth(self, query_auth=query_auth, sign_payload=sign_payload,
+            ctx = signer.add_auth(self, query_auth=query_auth, sign_payload=sign_payload,
                                   headers_not_to_sign=headers_not_to_sign, request_date=request_date, signing_date=signing_date)
-            return res
+
+        return ctx
 
     def _make_post_body_headers(self):
 
@@ -83,24 +90,27 @@ class Request(FixedKeysDict):
         for k, v in fields.iteritems():
             multipart_fields.append({'name': k, 'value': v})
 
-        if self['do_add_auth'] is True:
+        if self['do_add_auth']:
             if content is None:
                 multipart_fields.append({
                     'name': 'file',
                     'value': '',
                     })
+
             elif isinstance(content, str):
                 multipart_fields.append({
                     'name': 'file',
                     'value': content,
                     })
+
             elif isinstance(content, file):
                 multipart_fields.append({
                     'name': 'file',
                     'value': [content, os.fstat(content.fileno()).st_size, os.path.basename(content.name)],
                     })
+
             else:
-                raise InvalidArgumentError('content to upload must be a file or str')
+                raise InvalidArgumentError('type of content {x} is not str or file'.format(x=type(content)))
 
         body_reader = multipart_cli.make_body_reader(multipart_fields)
         data = []
