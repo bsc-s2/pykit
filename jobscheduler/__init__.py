fd255281233f2ea8ea602aef6090b46d854efe89
drdr xp <drdr.xp@gmail.com>
2020-02-03 14:47:26 +0800

    py2: jobscheduler: fix: unittest should assume it runs with various local-timezone, instead of only Asia/Shanghai

--- jobscheduler/__init__.py
+++ jobscheduler/__init__.py
@@ -1,9 +1,7 @@
-from .jobscheduler import (
-    JobExistError,
-    JobScheduler,
-    NextFireTimeError,
-    get_next_fire_time,
-)
+from .jobscheduler import JobExistError
+from .jobscheduler import JobScheduler
+from .jobscheduler import NextFireTimeError
+from .jobscheduler import get_next_fire_time
 
 __all__ = [
     'JobExistError',
